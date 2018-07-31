207cf45a518f626184e7e790aa7841ae69038abf
liujiwei <liujiwei0529@163.com>
2018-07-31 16:41:25 +0800

    shiro通过注解配置权限

--- shiro-web_spring/src/main/java/net/abadguy/controller/UserAction.java
+++ shiro-web_spring/src/main/java/net/abadguy/controller/UserAction.java
@@ -3,6 +3,7 @@ package net.abadguy.controller;
 import net.abadguy.entity.User;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +29,20 @@ public class UserAction {
         }
 
         return "登录成功";
+    }
+
+
+    @RequiresRoles("admin")
+    @RequestMapping(value = "/testRole",method = RequestMethod.GET)
+    @ResponseBody
+    public String testRole(){
+        return "testRole success";
+    }
+
+    @RequiresRoles("admin1")
+    @RequestMapping(value = "/testRole1",method = RequestMethod.GET)
+    @ResponseBody
+    public String testRole1(){
+        return "testRole success";
     }
 }
